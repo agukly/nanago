83927b86b09cf26c0de378a1e85735b393abdaba
Birgit Klink <birgit@morla.rocks>
2020-03-03 11:23:51 +0800

    added pundit and extended prams controller for pundit

--- app/controllers/prams_controller.rb
+++ app/controllers/prams_controller.rb
@@ -1,10 +1,44 @@
 class PramsController < ApplicationController
-  def new
+  before_action :set_pram, only: [:show, :destroy]
+
+   def index
+    # @prams = Pram.all
+    @prams = policy_scope(Pram)
   end
 
   def show
+    authorize @pram
   end
 
-  def index
+  def new
+    @pram = Pram.new
+    authorize @pram
+  end
+
+  def create
+    @pram = Pram.new(pram_params)
+    @pram.user = current_user
+    authorize @pram
+    if @pram.save
+      redirect_to @pram, notice: 'Pram was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    authorize @pram
+    @pram.destroy
+    redirect_to pram_url, notice: 'Pram was successfully deleted.'
+  end
+
+  private
+
+  def set_pram
+    @pram = Pram.find(params[:id])
+  end
+
+  def pram_params
+    params.require(:pram).permit(:brand, :model, :location, :year, :description, :price)
   end
 end
