70b4bd54f2889837aaa3993c1b3f42efd9bc569c
Aga Klyz <klyz.aga@gmail.com>
2020-03-04 17:21:41 +0800

    mapbox.css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,4 @@
 import "bootstrap";
-import 'mapbox-gl/dist/mapbox-gl.css';
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import flatpickr from "flatpickr";
 import { initFlatPicker } from '../plugins/init_flatpickr';
 import { initMapbox } from '../plugins/init_mapbox';
