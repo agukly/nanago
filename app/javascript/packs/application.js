2eb85354fc7791295e30f235089d9c7c8da874b8
Birgit Klink <birgit@morla.rocks>
2020-03-04 16:20:30 +0800

    Flatdatepicker and total price included

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,9 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-
+import flatpickr from "flatpickr";
+import { initFlatPicker } from '../plugins/init_flatpickr';
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
+initFlatPicker();
