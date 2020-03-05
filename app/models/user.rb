18d0c98d4523d1b48e3c13904fc43660a1d241fe
Aga Klyz <klyz.aga@gmail.com>
2020-03-05 19:07:00 +0800

    we have chat now!(almost)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,18 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :prams
   has_many :reviews, through: :bookings
-  
   has_one_attached :photo
 
   validates :first_name, presence: true, length: { minimum: 2}
   validates :last_name, presence: true, length: { minimum: 2}
+
+  def messaging_with(conversation)
+    current_user = self
+    if current_user.id == conversation.sender_id
+      return User.find(conversation.recipient_id)
+    else
+      return User.find(conversation.sender_id)
+    end
+  end
+
 end
